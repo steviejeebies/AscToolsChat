3f8c994799ab24611222d3c6acd97f25bfd1c592
steviejeebies <rowes@tcd.ie>
2020-03-14 21:24:10 +0000

    Updates

---
+++ precache-manifest.ee329ad5cfd8ec238850c0d37d007f88.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09ed1f13f08847793a194695ce9c8bce",
+    "url": "/AscToolsChat/index.html"
+  },
+  {
+    "revision": "954f5c3f6292350c0002",
+    "url": "/AscToolsChat/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "9d4406a2032e529bd2c8",
+    "url": "/AscToolsChat/static/css/main.961f6289.chunk.css"
+  },
+  {
+    "revision": "954f5c3f6292350c0002",
+    "url": "/AscToolsChat/static/js/2.4c1f0a5f.chunk.js"
+  },
+  {
+    "revision": "04a493a28472d7736e523968fadd041b",
+    "url": "/AscToolsChat/static/js/2.4c1f0a5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d4406a2032e529bd2c8",
+    "url": "/AscToolsChat/static/js/main.d1d3537a.chunk.js"
+  },
+  {
+    "revision": "aa95ecaf488cf0ebbca0",
+    "url": "/AscToolsChat/static/js/runtime-main.c9f8104a.js"
+  },
+  {
+    "revision": "44dda33d86723197cb26ad73010d03d7",
+    "url": "/AscToolsChat/static/media/logo-transparent.44dda33d.svg"
+  }
+]);
