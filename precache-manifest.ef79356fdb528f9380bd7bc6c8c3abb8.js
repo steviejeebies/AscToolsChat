a6e4c452e73c42ed4d867d447f9e43be21cb11cd
steviejeebies <rowes@tcd.ie>
2020-03-25 08:36:28 +0000

    Updates

---
+++ precache-manifest.ef79356fdb528f9380bd7bc6c8c3abb8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "637adab46679e4e3e6a7770ad4ec5eee",
+    "url": "/Home/index.html"
+  },
+  {
+    "revision": "4dd794d80fa0aacf44c3",
+    "url": "/Home/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "9fa9e4059cadbee1029e",
+    "url": "/Home/static/css/main.34a261eb.chunk.css"
+  },
+  {
+    "revision": "4dd794d80fa0aacf44c3",
+    "url": "/Home/static/js/2.303354bb.chunk.js"
+  },
+  {
+    "revision": "5bbcfa92673046555caf7e043f730bc1",
+    "url": "/Home/static/js/2.303354bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fa9e4059cadbee1029e",
+    "url": "/Home/static/js/main.846b8169.chunk.js"
+  },
+  {
+    "revision": "48a1b947c7a41710948d",
+    "url": "/Home/static/js/runtime-main.60b75a19.js"
+  },
+  {
+    "revision": "5bb53262cd8dea64e3ce2c3608bd48b1",
+    "url": "/Home/static/media/boatImage.5bb53262.jpg"
+  },
+  {
+    "revision": "44dda33d86723197cb26ad73010d03d7",
+    "url": "/Home/static/media/logo-vector.44dda33d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b75b29046483873a9fbf98a40a86eff2.js"
+  "/Home/precache-manifest.ef79356fdb528f9380bd7bc6c8c3abb8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Home/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
