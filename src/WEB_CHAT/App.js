09dea3b52b72771f8c9fde6e22936eab66eba9e7
unknown <rowes@tcd.ie>
2020-04-13 01:20:21 +0100

    restructured the app a bit, I've merged App.js and AscToolsWebChat.js into the one file.

--- src/WEB_CHAT/App.js
+++ src/WEB_CHAT/App.js
@@ -1,14 +1,22 @@
-import { Components } from 'botframework-webchat-component';
-import { createDirectLine, createStore } from 'botframework-webchat';
+// import React libraries
 import React, { useState, useEffect } from 'react';
-import AscToolsWebChat from "./AscToolsWebChat";
-import directLineDisconnect from 'botframework-webchat-core/lib/actions/disconnect';
 
+// import libraries from Microsoft's botframework-webchat repo
+import { createDirectLine, createStore } from 'botframework-webchat';
+import directLineDisconnect from 'botframework-webchat-core/lib/actions/disconnect';
+import { Components } from 'botframework-webchat-component';
+
+// import from other files in our AscTools project
+import ChatInput from "./ChatInput.js";
+import MessageList from "./MessageList.js";
+
+// import from React-Boostrap
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 
 var secret = process.env.DIRECT_LINE_SECRET;
 
-// ORIGINAL
+// OLD CODE
 // async function getDirectLineToken() {
 //   const res = await fetch('https://directline.botframework.com/v3/directline/tokens/generate', { method: 'POST', 
 // //  mode: 'no-cors',
@@ -22,22 +30,20 @@ var secret = process.env.DIRECT_LINE_SECRET;
 // }
 
 const initializeDirectLine = async setDirectLine => {
-  const res = await fetch('https://directline.botframework.com/v3/directline/tokens/generate', { method: 'POST', 
-  //  mode: 'no-cors',
-    headers: {
-  //  'Content-Type': 'application/json',
-        'Authorization': 'Bearer iQ0RbsV7tfA.Iv9w0NYvH7OoZJxqilYyZMdbK-xLhYlco0piDalWT00'
-        }});
+  const res = await fetch('https://directline.botframework.com/v3/directline/tokens/generate', 
+                { method: 'POST', headers: { 'Authorization': 'Bearer iQ0RbsV7tfA.Iv9w0NYvH7OoZJxqilYyZMdbK-xLhYlco0piDalWT00' }});
   const { token } = await res.json();
   setDirectLine(createDirectLine({ token }));
 };
 
 function App() {
+  // Establising connection to the bot
   const [directLine, setDirectLine] = useState();
   useEffect(() => {
     initializeDirectLine(setDirectLine);
   }, []);
 
+  // this method is used for the New Patient button, allows us to restart the conversation
   const storeMiddleware = () => next => action => {
     if (action.type === 'DIRECT_LINE/DISCONNECT_FULFILLED') {
       setDirectLine(null);
@@ -47,13 +53,28 @@ function App() {
     return next(action)
   };
 
+  // store is a state used by the imported 'Components' library
   const [store, setStore] = useState(createStore({}, storeMiddleware));
-
-  console.log(store);
 
   const disconnect = () => store.dispatch(directLineDisconnect());
 
-// ORIGINAL
+  // The following code is used for resizing the window. This was necessary in order for the
+  // app to be fully responsive (as well as display correctly on mobile), as simply using vh
+  // variables in the CSS files did not achieve the desired result
+  let vh = window.innerHeight * 0.01;
+	let vw = window.innerWidth * 0.01;
+	// Then we set the value in the --vh custom property to the root of the document
+	document.documentElement.style.setProperty('--vh', `${vh}px`);
+	document.documentElement.style.setProperty('--vw', `${vw}px`);
+
+	window.addEventListener('resize', () => {
+		// We execute the same script as before
+		let vh = window.innerHeight * 0.01;
+		let vw = window.innerWidth * 0.01;
+		document.documentElement.style.setProperty('--vh', `${vh}px`);
+		document.documentElement.style.setProperty('--vw', `${vw}px`);
+	  });
+// OLD CODE
 //   const [directLine, setDirectLine] = React.useState();
 
 //   if (!directLine) {
@@ -61,15 +82,18 @@ function App() {
 //     getDirectLineToken().then(token => setDirectLine(createDirectLine({ token })));
 //   }
 
-// // The following return value is just to let us know if we are connected to the MockBot
-// // When running the app, it should switch from "Not Connected!" to "Connected!" quickly
-
   return (
-	<div>
-    <Button onClick={disconnect}>Button</Button>
-        <AscToolsWebChat directLine={directLine} store={store}/>
-      )}
-	</div>
+    <div className="WindowBackground">
+      <Button onClick={disconnect}>New Patient</Button>
+          {!!directLine &&
+            <Components.Composer directLine={directLine} store={store}>
+              <div className="ChatContentContainer">
+                <MessageList/>
+                <ChatInput />
+              </div>
+            </Components.Composer>
+          }
+    </div>
   );
 }
 
