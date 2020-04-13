1012574ba0e6d942a659f7a9cb356903dc66b9c3
unknown <rowes@tcd.ie>
2020-04-13 14:55:00 +0100

    text that the bot sends with prompt buttons is now displayed

--- src/WEB_CHAT/TextBox.js
+++ src/WEB_CHAT/TextBox.js
@@ -19,17 +19,26 @@ function TextBox(props) {
         userName = "You";
         thisStyle = {alignSelf: "flex-end", flexShrink:0, minWidth:"100px", marginRight: "1vw"};
     }
-    const sendMessageBack = useSendMessageBack();
 
+    // For dealing specifically with messages that have prompt buttons
     // This is boolean that checks if the message object passed has attachments at all
     const shouldIncludeButtonPrompts = (props.message.hasOwnProperty("attachments")) && !!props.message.attachments;
+
+    // if it is a message that has prompts, then we need to specify which object from the JSON object 
+    // sent from the bot we should use as the text content of the message
+    if(shouldIncludeButtonPrompts)
+        text = props.message.attachments[0].content.title;
+    else 
+        text = props.message.text;
+
+    const sendMessageBack = useSendMessageBack();
     
     // This method will extract the innerHTML from a prompt button that was pressed, and will send it
     // The most important parameter here going forward will be the first parameter, which is just a 
     // fake object at the moment, but it will allow us to send objects of any type to the bot 
     const sendPromptReply = event => {
         event.preventDefault();
-        sendMessageBack({object:33, test:22}, event.target.innerHTML, event.target.innerHTML);
+        sendMessageBack({testingThatWeCanSendAnObjectFromScratch: 1}, event.target.innerHTML, event.target.innerHTML);
     }
 
     // This will make a list of buttons to display, but only if the above shouldIncludeButtonPrompts is true
